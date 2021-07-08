c7e5b4c3861725d23813f38b0f3bd26d77b956a9
Tomas Motiejūnas <tom.motiejunas@gmail.com>
2021-07-08 20:09:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/*const weatherData = async function () {
+  try {
+    const response = await fetch(
+      "https://weatherapi-com.p.rapidapi.com/forecast.json?q=Vilnius&days=3",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key":
+            "d31113971cmshf0588227f767bd5p1ebe58jsn2531b1567fb5",
+          "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data.forecast);
+  } catch (err) {
+    console.log(err);
+  }
+};
+weatherData();
+*/
